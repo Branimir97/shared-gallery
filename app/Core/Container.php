98514995c3f70e901b6c6a407fab8580ad7739c6
Branimir97 <branimirb51@gmail.com>
2021-09-19 17:08:57 +0200

    Instantiated .gitignore | Generated container

--- app/Core/Container.php
+++ app/Core/Container.php
@@ -2,7 +2,44 @@
 
 namespace Core;
 
-class Container
+class Container implements \ArrayAccess
 {
+    public $items = [];
+    public $cacheItems = [];
 
+    public function offsetExists($offset): bool
+    {
+        return isset($this->items[$offset]);
+    }
+
+    public function offsetGet($offset)
+    {
+        if(!($this->offsetExists($offset))) {
+            return null;
+        }
+        if(isset($this->cacheItems[$offset])) {
+            return $this->cacheItems[$offset];
+        } else {
+            $item = $this->items[$offset]($this);
+            $this->cacheItems[$offset] = $item;
+            return $item;
+        }
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        $this->items[$offset] = $value;
+    }
+
+    public function offsetUnset($offset)
+    {
+        if($this->offsetExists($offset)) {
+            unset($this->items[$offset]);
+            unset($this->cacheItems[$offset]);
+        }
+    }
+
+    public function __get($property) {
+        return $this->offsetGet($property);
+    }
 }
--- app/index.php
+++ app/index.php
@@ -6,7 +6,28 @@ use Core\App;
 
 $app = new App;
 
-var_dump($app->getContainer());
+$container = $app->getContainer();
+
+$container['env'] = function () {
+    return [
+        'db_driver' => 'mysql',
+        'db_host' => 'localhost',
+        'db_name' => 'inchoo-task',
+        'db_user' => 'root',
+        'db_password' => 'admin'
+    ];
+};
+
+$container['database'] = function ($c) {
+    $env = $c->env;
+    return new PDO(
+        $c->env['db_driver'].":host=".$c->env['db_host'].";dbname=".$c->env['db_name'],
+        $c->env['db_user'],
+        $c->env['db_password']
+    );
+};
+
+var_dump($container->database);
 
 
 
