5812b6b28652370ff96e7c92a1ed6718f022f6d1
Branimir97 <branimirb51@gmail.com>
2021-11-14 20:06:21 +0100

    Deleted duplicated code

--- app/Views/AccountView.php
+++ app/Views/AccountView.php
@@ -1,87 +1,19 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" 
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
-          crossorigin="anonymous">
-
-    <!-- FontAwesome -->
-    <script src="https://kit.fontawesome.com/6aa1bd9ffa.js" crossorigin="anonymous"></script>
+    <?php include "base/libraries.php"?>
 
     <!-- CSS -->
     <style>
-          <?php include "css/container.css"?>
+      <?php include "css/container.css"?>
     </style>
 
-    <!-- Icon -->
-    <link rel="icon" href="../Public/Icons/logo.png">
-    
     <title>
       Account details
     </title>
   </head>
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="/home">
-        Shared gallery
-        <i class="fas fa-camera-retro"></i>
-      </a>
-      <button class="navbar-toggler" type="button" 
-              data-toggle="collapse" data-target="#navbarNavAltMarkup" 
-              aria-controls="navbarNavAltMarkup" aria-expanded="false" 
-              aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav ml-auto">
-          <a class="nav-item nav-link" href="/home">
-            Home
-            <i class="fas fa-house-damage"></i>
-          </a>
-          <?php 
-            if(isset($_SESSION['loggedIn'])): ?>
-            <li class="nav-item dropdown active">
-              <a class="nav-link dropdown-toggle" href="#" 
-                id="navbarDropdownMenuLink" data-toggle="dropdown" 
-                aria-haspopup="true" aria-expanded="false">
-                Menu
-                <i class="fas fa-chevron-circle-down"></i>
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a class="dropdown-item" href="/management">
-                  Management
-                  <i class="fas fa-images"></i>
-                </a>
-                <a class="dropdown-item active" href="/account">
-                  My account
-                  <i class="fas fa-user-circle"></i>
-                </a>
-              </div>
-            </li>
-            <a class="nav-item nav-link" href="/logout">
-              Logout
-              <i class="fas fa-sign-in-alt"></i>
-            </a>
-            <?php else: ?>
-            <a class="nav-item nav-link" href="/login">
-              Login
-              <i class="fas fa-sign-in-alt"></i>
-            </a>
-            <a class="nav-item nav-link" href="/register">
-              Register
-              <i class="fas fa-user-plus"></i>
-            </a>
-          <?php endif; ?>
-        </div>
-      </div>
-    </nav>
-    
+  <body>   
     <div class="container text-center mt-2">        
       <p>
           You're logged in as 
@@ -177,20 +109,5 @@
         </div>
       </div>
     </div>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
-            crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" 
-            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" 
-            crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" 
-            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" 
-            crossorigin="anonymous">
-    </script>
   </body>
 </html>
--- app/Views/HomeView.php
+++ app/Views/HomeView.php
@@ -1,82 +1,15 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" 
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
-          crossorigin="anonymous">
-
-    <!-- FontAwesome -->
-    <script src="https://kit.fontawesome.com/6aa1bd9ffa.js" crossorigin="anonymous"></script>
-
-    <!-- Icon -->
-    <link rel="icon" href="../Public/Icons/logo.png">
+    <?php include('base/libraries.php'); ?>
 
     <title>
       Shared gallery
     </title>
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="/home">
-        Shared gallery
-        <i class="fas fa-camera-retro"></i>
-      </a>
-      <button class="navbar-toggler" type="button" 
-              data-toggle="collapse" data-target="#navbarNavAltMarkup" 
-              aria-controls="navbarNavAltMarkup" aria-expanded="false" 
-              aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav ml-auto">
-          <a class="nav-item nav-link active" href="/home">
-            Home
-            <i class="fas fa-house-damage"></i>
-          </a>
-          <?php 
-            if(isset($_SESSION['loggedIn'])): ?>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" 
-                id="navbarDropdownMenuLink" data-toggle="dropdown" 
-                aria-haspopup="true" aria-expanded="false">
-                Menu
-                <i class="fas fa-chevron-circle-down"></i>
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a class="dropdown-item" href="/management">
-                  Management
-                  <i class="fas fa-images"></i>
-                </a>
-                <a class="dropdown-item" href="/account">
-                  My account
-                  <i class="fas fa-user-circle"></i>
-                </a>
-              </div>
-            </li>
-            <a class="nav-item nav-link" href="/logout">
-              Logout
-              <i class="fas fa-sign-in-alt"></i>
-            </a>
-            <?php else: ?>
-            <a class="nav-item nav-link" href="/login">
-              Login
-              <i class="fas fa-sign-in-alt"></i>
-            </a>
-            <a class="nav-item nav-link" href="/register">
-              Register
-              <i class="fas fa-user-plus"></i>
-            </a>
-          <?php endif; ?>
-        </div>
-      </div>
-    </nav>
-    
+    <?php include('base/navbar.php'); ?>    
     <div class="container text-center mt-2">
       <?php
         if(isset($_SESSION['loggedInMessage'])) {
@@ -85,8 +18,8 @@
               '.$_SESSION['loggedInMessage'].'
             </p>
           ';
+          unset($_SESSION['loggedInMessage']);
         }
-        unset($_SESSION['loggedInMessage']);
       ?>
       <button class="trigger p-2 btn btn-outline-success">
           Get total number of 
@@ -98,21 +31,6 @@
       <div class="result mt-3"></div>
     </div>
     
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then AJAX, then Popper.js, then Bootstrap JS, then Ajax Call -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
-            crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" 
-            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" 
-            crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" 
-            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" 
-            crossorigin="anonymous">
-    </script>
     <script>
       <?php include('Services/CountPhotos.js') ?>
     </script>
